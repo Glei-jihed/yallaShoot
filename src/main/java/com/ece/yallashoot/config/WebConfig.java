16203b41e10dbd4e91a4f9fbd7cb5d802a3d3bce
Glei-jihed <glei.jihed@gmail.com>
2024-02-03 14:09:14 +0100

    web config commit

--- src/main/java/com/ece/yallashoot/config/WebConfig.java
+++ src/main/java/com/ece/yallashoot/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization","Bearer Token")
                 .allowCredentials(true);
     }
 }
